d6ca24677cb580258ee452c8790ec16e055e0a19
zstardustttt <93951756+zSt4rdust@users.noreply.github.com>
2025-07-06 15:00:34 +0700

    speed affects fov

--- Assets/_Project/Scripts/Player/OnlinePlayer.cs
+++ Assets/_Project/Scripts/Player/OnlinePlayer.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Game.Player
@@ -6,8 +8,12 @@ namespace Game.Player
     public class OnlinePlayer : MonoBehaviour, IPlayerController
     {
         public PlayerMovement movement;
+        public int speedRecordSize;
+
+        private Queue<float> _speedRecord;
 
         [Header("Speedlines")]
+        public bool enableSpeedlines;
         public Transform speedlines;
         public Material speedlinesFullscreenMaterial;
         public float minSpeedlinesSpeed;
@@ -15,10 +21,19 @@ namespace Game.Player
         public AnimationCurve speedlinesAlphaCurve;
         public float speedlinesAlphaSmoothingSpeed;
 
+        private float _currentSpeedlinesAlpha;
+
+        [Header("Speed Affects FOV")]
+        public bool enableSpeedAffectsFOV;
+        public float idleFOV;
+        public float maxFOV;
+        public float maxFOVSpeed;
+        public AnimationCurve FOVCurve;
+        public float FOVSmoothingSpeed;
+
         private Camera _camera;
         private float _cameraRotX;
         private Vector3 _prevPosition;
-        private float _targetSpeedlinesAlpha;
 
         private void OnValidate()
         {
@@ -27,6 +42,7 @@ namespace Game.Player
 
         private void Awake()
         {
+            _speedRecord = new(speedRecordSize);
             movement.controller = this;
             Cursor.lockState = CursorLockMode.Locked;
             _camera = Camera.main;
@@ -34,6 +50,7 @@ namespace Game.Player
 
         private void Update()
         {
+            // CAMERA ROTATION
             var delta = Input.mousePositionDelta * 0.2f;
             movement.orientation.localEulerAngles += new Vector3(0f, delta.x, 0f);
             _cameraRotX -= delta.y;
@@ -45,21 +62,40 @@ namespace Game.Player
                 0f
             );
 
-            var dir = transform.position - _prevPosition;
-            var speed = dir.magnitude / Time.deltaTime;
+            // FIND SPEED
+            var vel = transform.position - _prevPosition;
+            var rawSpeed = vel.magnitude / Time.deltaTime;
+            var dir = vel.normalized;
 
-            if (speed >= minSpeedlinesSpeed)
+            if (_speedRecord.Count == speedRecordSize) _speedRecord.Dequeue();
+            _speedRecord.Enqueue(rawSpeed);
+            var speed = _speedRecord.ToArray().Average();
+
+            // FOV
+            if (enableSpeedAffectsFOV)
             {
-                dir.Normalize();
-
-                var alpha = speedlinesAlphaCurve.Evaluate((speed - minSpeedlinesSpeed) / maxSpeedlinesSpeed);
-                _targetSpeedlinesAlpha = Mathf.Lerp(_targetSpeedlinesAlpha, alpha, Time.deltaTime * speedlinesAlphaSmoothingSpeed);
-
-                speedlines.transform.SetPositionAndRotation(_camera.transform.position + dir * 2.3f, Quaternion.LookRotation(-dir));
+                var dot = Vector3.Dot(_camera.transform.forward, dir);
+                var targetFov = Mathf.Lerp(idleFOV, maxFOV, FOVCurve.Evaluate(speed / maxFOVSpeed * Mathf.Abs(dot)));
+                _camera.fieldOfView = Mathf.Lerp(_camera.fieldOfView, targetFov, Time.deltaTime * FOVSmoothingSpeed);
             }
-            else _targetSpeedlinesAlpha = Mathf.Lerp(_targetSpeedlinesAlpha, 0f, Time.deltaTime * speedlinesAlphaSmoothingSpeed);
+            else _camera.fieldOfView = idleFOV;
 
-            speedlinesFullscreenMaterial.SetFloat("_alpha", _targetSpeedlinesAlpha);
+            // SPEEDLINES
+            if (enableSpeedlines)
+            {
+                if (speed >= minSpeedlinesSpeed)
+                {
+                    var targetAlpha = speedlinesAlphaCurve.Evaluate((speed - minSpeedlinesSpeed) / maxSpeedlinesSpeed);
+                    _currentSpeedlinesAlpha = Mathf.Lerp(_currentSpeedlinesAlpha, targetAlpha, Time.deltaTime * speedlinesAlphaSmoothingSpeed);
+
+                    speedlines.transform.SetPositionAndRotation(_camera.transform.position + dir * 2.3f, Quaternion.LookRotation(-dir));
+                }
+                else _currentSpeedlinesAlpha = Mathf.Lerp(_currentSpeedlinesAlpha, 0f, Time.deltaTime * speedlinesAlphaSmoothingSpeed);
+
+                speedlinesFullscreenMaterial.SetFloat("_alpha", _currentSpeedlinesAlpha);
+            }
+            else speedlinesFullscreenMaterial.SetFloat("_alpha", 0f);
+
             _prevPosition = transform.position;
         }
 
