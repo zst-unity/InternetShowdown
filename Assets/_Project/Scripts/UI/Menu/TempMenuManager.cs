70df355dd1fcd138bff2b150703e9512c6502cef
zstardustttt <93951756+zSt4rdust@users.noreply.github.com>
2025-07-07 11:41:13 +0700

    update temp

--- Assets/_Project/Scripts/UI/Menu/TempMenuManager.cs
+++ Assets/_Project/Scripts/UI/Menu/TempMenuManager.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class TempMenuManager : MonoBehaviour
 {
+    private void Awake()
+    {
+        PlayerPrefs.SetFloat("sens", 1f);
+    }
+
     public void SetSens(float value)
     {
         PlayerPrefs.SetFloat("sens", value);
